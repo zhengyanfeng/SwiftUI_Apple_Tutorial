495d41b141a97e460ac51eb090147bb8e1432996
zhengyanfeng <zhengyanfeng@haylou.com>
2021-01-26 17:37:08 +0800

    add scrumsView list view

---
+++ Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -0,0 +1,35 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by liesheng on 2021/1/26.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                NavigationLink(
+                    destination: Text(scrum.title)) {
+                    
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.color)
+            }
+        }
+        .navigationTitle("Daily Scrums")
+        .navigationBarItems(trailing: Button(action: {}, label: {
+            Image(systemName: "plus")
+        }))
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.data)
+    }
+}
