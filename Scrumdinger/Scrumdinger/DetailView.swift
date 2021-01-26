01a9b64ea9baf6345b9b19e3ff5d1dc574787205
zhengyanfeng <zhengyanfeng@haylou.com>
2021-01-26 18:45:08 +0800

    add navigation view and push to next view

---
+++ Scrumdinger/Scrumdinger/DetailView.swift
@@ -0,0 +1,57 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by liesheng on 2021/1/26.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                NavigationLink(
+                    destination: MeetingView()){
+                    Label("Start Meeting", image: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                        .accessibilityLabel(Text("Start meeting"))
+                }
+                
+                HStack {
+                    Label("Length", systemImage: "clock")
+                        .accessibilityLabel(Text("Meeting length"))
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                
+                HStack{
+                    Label("Color", systemImage: "paintpalette")
+                    Spacer()
+                    Image(systemName: "checkmark.circle.fill")
+                }
+                .accessibilityElement(children: .ignore)
+            }
+            
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees, id: \.self) { attendee in
+                    Label(attendee, systemImage: "person")
+                        .accessibilityLabel(Text("Person"))
+                        .accessibilityValue(Text(attendee))
+                }
+            }
+        }
+        .listStyle(InsetGroupedListStyle())
+        .navigationTitle(scrum.title)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DetailView(scrum: DailyScrum.data[0])
+        }
+    }
+}
